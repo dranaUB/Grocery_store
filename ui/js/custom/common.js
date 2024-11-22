9aa7eda8d8eac1b4649ea8af005a9875a0e50c16
Devendra Rana <32830074+dranaUB@users.noreply.github.com>
2024-11-22 18:01:00 -0500

    Update common.js
    for ssh

--- ui/js/custom/common.js
+++ ui/js/custom/common.js
@@ -1,11 +1,11 @@
 // Define your api here
-var productListApiUrl = 'http://127.0.0.1:5000/getProducts';
-var uomListApiUrl = 'http://127.0.0.1:5000/getUOM';
-var productSaveApiUrl = 'http://127.0.0.1:5000/insertProduct';
-var productDeleteApiUrl = 'http://127.0.0.1:5000/deleteProduct';
-var orderListApiUrl = 'http://127.0.0.1:5000/getAllOrders';
-var orderSaveApiUrl = 'http://127.0.0.1:5000/insertOrder';
-
+var baseApiUrl = 'https://ubgroceryportal-bcf9fac0hzdvhud8.canadacentral-01.azurewebsites.net/'; // Replace with your actual Azure App Service URL
+var productListApiUrl = `${baseApiUrl}/getProducts`;
+var uomListApiUrl = `${baseApiUrl}/getUOM`;
+var productSaveApiUrl = `${baseApiUrl}/insertProduct`;
+var productDeleteApiUrl = `${baseApiUrl}/deleteProduct`;
+var orderListApiUrl = `${baseApiUrl}/getAllOrders`;
+var orderSaveApiUrl = `${baseApiUrl}/insertOrder`;
 // For product drop in order
 var productsApiUrl = 'https://fakestoreapi.com/products';
 

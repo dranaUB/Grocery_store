3e2232240ecd35f03d0f5b0d2548b889685705c8
Devendra Rana <32830074+dranaUB@users.noreply.github.com>
2024-11-23 04:49:26 -0500

    Update server.py
    Flask-Migrate try

--- backend/server.py
+++ backend/server.py
@@ -5,9 +5,26 @@ import json
 import products_dao
 import orders_dao
 import uom_dao
+import os
+
+# Configure Postgres database based on connection string of the libpq Keyword/Value form
+# https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+
+DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=conn_str_params['user'],
+    dbpass=conn_str_params['password'],
+    dbhost=conn_str_params['host'],
+    dbname=conn_str_params['dbname']
+)
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 connection = get_sql_connection()
 
 @app.route('/getUOM', methods=['GET'])
